528f96f442c65fee34119122cde08f2c5701b1ba
AsociTon <asociton@outlook.com>
2018-12-19 18:54:07 +0530

    created header.html and included home.html using includes

--- mysite/testapp/views.py
+++ mysite/testapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Test App<h1>")
-    
+    return render(request, 'testapp/home.html')
+    #return HttpResponse("<h1>Test App<h1>")
