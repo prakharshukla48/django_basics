b5305ea34ebdc0c75e97c7a064b5eea444e729e4
AsociTon <asociton@outlook.com>
2018-12-21 21:08:47 +0530

    fianlised connecting django via admin and PostGres

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=140)
     body = models.TextField()
     date = models.DateTimeField()
 
